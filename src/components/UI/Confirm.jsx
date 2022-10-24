550bc7f5251a98688a437de2282803f1940d086a
Nuraiym <nabdirasulkyzy@gmail.com>
2022-10-24 15:16:23 +0500

    nuraika

---
+++ src/components/UI/Confirm.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ErrorModall from "./error-module/ErrorModul";
+
+const Confirm = ({
+  isSure,
+  changeIsSure,
+  isTrue,
+  filteredHandler,
+  getId,
+  closeModalHandler,
+}) => {
+  return (
+    <>
+      {isSure &&
+        ReactDOM.createPortal(
+          <ErrorModall
+            {...isSure}
+            onConfirm={changeIsSure}
+            isTrue={isTrue}
+            filteredHandler={filteredHandler}
+            setId={getId}
+            closeAnimationHandler={closeModalHandler}
+          />,
+          document.getElementById("confirm-root")
+        )}
+    </>
+  );
+};
+
+export default Confirm;
